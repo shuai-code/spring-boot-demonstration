229dd7202eed5e72e24bc76036274218e65c9a21
Yangs <ys@keyike.ltd>
2022-06-21 15:38:42 +0800

    初始化SpringBoot项目

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-boot-demonstration
+
+用于添加和记录一些SpringBoot的使用技巧和方法
---
+++ src/main/java/com/shuai/demonstration/DemonstrationApplication.java
@@ -0,0 +1,14 @@
+package com.shuai.demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yangs
+ */
+@SpringBootApplication
+public class DemonstrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemonstrationApplication.class, args);
+    }
+}
