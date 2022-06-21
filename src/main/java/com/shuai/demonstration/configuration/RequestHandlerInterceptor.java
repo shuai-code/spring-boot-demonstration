ce1355706af275be32725d95f971f432aade172d
Yangs <ys@keyike.ltd>
2022-06-21 16:38:18 +0800

    自定义SpringBoot的接口拦截器

---
+++ src/main/java/com/shuai/demonstration/configuration/RequestHandlerInterceptor.java
@@ -0,0 +1,34 @@
+package com.shuai.demonstration.configuration;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 一个接口拦截器, 在每个接口请求到达业务逻辑之前做一些处理
+ * @author Yangs
+ */
+@Slf4j
+@Component
+public class RequestHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info("收到接口请求URI: {}, 请求参数: {}", request.getRequestURI(), JSONObject.toJSONString(request.getParameterMap()));
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+    }
+}
---
+++ src/main/java/com/shuai/demonstration/configuration/RequestWebMvcConfigurationSupport.java
@@ -0,0 +1,17 @@
+package com.shuai.demonstration.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author Yangs
+ */
+@Configuration
+public class RequestWebMvcConfigurationSupport extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestHandlerInterceptor());
+    }
+}
