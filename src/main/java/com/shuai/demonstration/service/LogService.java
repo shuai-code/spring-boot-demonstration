5a2d44de9b2f52182cf0d465071d8e3f4b9c96cb
Yangs <ys@keyike.ltd>
2022-09-26 16:56:43 +0800

    移除接口

--- src/main/java/com/shuai/demonstration/service/LogService.java
+++ src/main/java/com/shuai/demonstration/service/LogService.java
@@ -1,12 +1,23 @@
 package com.shuai.demonstration.service;
 
+import com.shuai.demonstration.annotation.RunTime;
+import com.shuai.demonstration.annotation.RunTimeClass;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 /**
  * @author Yangs
  */
-public interface LogService {
-    /**
-     * 添加日志
-     * @param message 日志内容
-     */
-    void addLog(String message);
+@RunTimeClass
+@Slf4j
+@Service
+public class LogService {
+
+    @RunTime
+    public void addLog(String message) {
+        log.info("方法执行......");
+        if ("".equals(message)) {
+            throw new RuntimeException("异常.....");
+        }
+    }
 }
